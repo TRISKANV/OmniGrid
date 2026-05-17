c96ff49b432eda2d4411c7329b877574d88f5ad2
TRISKANV <borrarcuenta0800@gmail.com>
2026-05-17 20:07:42 -0300

    Initialize settings.gradle.kts for project configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "OmniGrid"
+
+include(":app")
+include(":core:designsystem")
+include(":core:executor")
