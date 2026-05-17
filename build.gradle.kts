8c636fe22ff8127bf9418ea51c7291873742f45f
TRISKANV <borrarcuenta0800@gmail.com>
2026-05-17 20:08:13 -0300

    Add initial Gradle configuration for Android project

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Archivo Gradle raíz
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
