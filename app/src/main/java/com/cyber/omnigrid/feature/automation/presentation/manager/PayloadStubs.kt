30d2558d8464e9812d01a28e1284733975d326f4
TRISKANV <borrarcuenta0800@gmail.com>
2026-05-19 15:33:03 -0300

    Modify PayloadViewModel to include repository parameter
    Added a repository parameter to the PayloadViewModel constructor.

--- app/src/main/java/com/cyber/omnigrid/feature/automation/presentation/manager/PayloadStubs.kt
+++ app/src/main/java/com/cyber/omnigrid/feature/automation/presentation/manager/PayloadStubs.kt
@@ -3,7 +3,8 @@ package com.cyber.omnigrid.feature.automation.presentation.manager
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 
-class PayloadViewModel : ViewModel()
+// Le agregamos el parámetro al constructor que exige la MainActivity
+class PayloadViewModel(val repository: Any) : ViewModel()
 
 @Composable 
 fun PayloadListScreen(viewModel: PayloadViewModel, onNavigateToEditor: (String) -> Unit) {}
