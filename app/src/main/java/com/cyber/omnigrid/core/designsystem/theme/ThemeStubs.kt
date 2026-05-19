f8bcabdcb96b543169bc609498ddb847d96e97aa
TRISKANV <borrarcuenta0800@gmail.com>
2026-05-19 15:33:53 -0300

    Add ThemeStubs with OmniGridTheme and TrueBlack color

---
+++ app/src/main/java/com/cyber/omnigrid/core/designsystem/theme/ThemeStubs.kt
@@ -0,0 +1,11 @@
+package com.cyber.omnigrid.core.designsystem.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val TrueBlack = Color(0xFF000000)
+
+@Composable
+fun OmniGridTheme(content: @Composable () -> Unit) {
+    content()
+}
