d71be8cc3325721d4f3f3809a37fb04cfc1e8044
TRISKANV <borrarcuenta0800@gmail.com>
2026-05-19 12:26:07 -0300

    Create DashboardStubs.kt

---
+++ app/src/main/java/com/cyber/omnigrid/feature/dashboard/presentation/DashboardStubs.kt
@@ -0,0 +1,6 @@
+package com.cyber.omnigrid.feature.dashboard.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable 
+fun OmniDashboardScreen(onNavigateToLiveExecution: (String) -> Unit, onNavigateToSettings: () -> Unit) {}
